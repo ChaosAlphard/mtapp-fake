14aa4415032dd85feb13692baf8592d5cf9a6ce4
ChaosAlphard <Alphard450@outlook.com>
2019-08-08 23:53:46 +0800

    Create Header Components

---
+++ src/components/header/BulletinDetail.vue
@@ -0,0 +1,160 @@
+<template>
+  <transition name="bulletin-detail">
+    <div class="bulletin-detail" v-show="isShow">
+
+      <div class="detail-wrapper">
+        <div class="main-wrapper" :style="detailBGStyle">
+          <div class="ico" :style="headLogoStyle">
+            <h3 class="name">{{poiInfo.name}}</h3>
+          </div>
+        </div>
+      </div>
+
+      <div class="close-wrapper">
+        <span class="icon icon-close" @click="closeBulletin()"></span>
+      </div>
+    </div>
+
+  </transition>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+
+export default class App extends Vue {
+}
+</script>
+
+<style scoped>
+/* 公告详情 样式 */ 
+.header .bulletin-detail {
+		width: 100%;
+		height: 100%;
+		position: absolute;
+		left: 0;
+		top: 0;
+		background: rgba(98, 98, 98, 0.8);
+		z-index: 999;
+	}
+	
+	.header .bulletin-detail .detail-wrapper {
+		width: 100%;
+		height: 100%;
+		padding: 43px 20px 125px;
+		box-sizing: border-box;
+	}
+	
+	.header .bulletin-detail .detail-wrapper .main-wrapper {
+		width: 100%;
+		height: 100%;
+		background-size: 100% 100%;
+		border-radius: 10px;
+		text-align: center;
+	}
+	
+	.header .bulletin-detail .detail-wrapper .main-wrapper .ico {
+		width: 60px;
+		height: 60px;
+		background-size: 135% 100%;
+		background-position: center;
+		border-radius: 5px;
+		display: inline-block;
+		margin-top: 40px;
+	}
+	
+	.header .bulletin-detail .detail-wrapper .main-wrapper .name {
+		font-size: 15px;
+		color: white;
+		margin-top: 13px;
+	}
+	
+	.header .bulletin-detail .detail-wrapper .main-wrapper .score {
+		height: 10px;
+		margin-top: 6px;
+		text-align: center;
+		font-size: 0;
+	}
+	
+	.header .bulletin-detail .detail-wrapper .main-wrapper .score .star {
+		display: inline-block;
+		margin-right: 7px;
+	}
+	
+	.header .bulletin-detail .detail-wrapper .main-wrapper .score span {
+		display: inline-block;
+		font-size: 10px;
+		color: white;
+	}
+	
+	.header .bulletin-detail .detail-wrapper .main-wrapper .tip {
+		font-size: 11px;
+		color: #bababc;
+		margin-top: 8px;
+	}
+	
+	.header .bulletin-detail .detail-wrapper .main-wrapper .tip i {
+		margin: 0 7px;
+	}
+	
+	.header .bulletin-detail .detail-wrapper .main-wrapper .time {
+		font-size: 11px;
+		color: #bababc;
+		margin-top: 13px;
+	}
+	
+	.header .bulletin-detail .detail-wrapper .main-wrapper .discounts {
+		margin-top: 20px;
+		padding: 0 20px;
+	}
+	
+	.header .bulletin-detail .detail-wrapper .main-wrapper .discounts p {
+		padding-top: 20px;
+		border-top: 1px solid #BABABC;
+	}
+	
+	.header .bulletin-detail .detail-wrapper .main-wrapper .discounts img {
+		width: 16px;
+		height: 16px;
+		vertical-align: middle;
+	}
+	
+	.header .bulletin-detail .detail-wrapper .main-wrapper .discounts span {
+		font-size: 11px;
+		line-height: 16px;
+		color: white;
+  }
+  
+  .header .bulletin-detail .detail-wrapper .close-wrapper {
+		padding-top: 20px;
+		height: 40px;
+		text-align: center;
+	}
+	
+	.header .bulletin-detail .detail-wrapper .close-wrapper span {
+  width: 40px;
+  height: 40px;
+  line-height: 40px;
+  border-radius: 50%;
+  font-size: 14px;
+  color: white;
+  display: inline-block;
+  background: rgba(118, 118, 118, 0.7);
+  border: 1px solid rgba(140, 140, 140, 0.9);
+}
+
+/* 动画效果 */ 
+.bulletin-detail-enter-active,
+.bulletin-detail-leave-active {
+  transition: 2s all;
+}
+
+.bulletin-detail-enter,
+.bulletin-detail-leave-to {
+  opacity: 0;
+}
+
+.bulletin-detail-enter-to,
+.bulletin-detail-leave {
+  opacity: 1;
+}
+</style>
---
+++ src/components/header/TopWrapper.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="top-wrapper">
+    <div class="back-wrapper">
+      <span class="icon icon-arrow-left"></span>
+    </div>
+
+    <form class="search-wrapper">
+      <span class="search-icon"></span>
+      <input type="text" class="search-bar" placeholder="搜索店内商品">
+    </form>
+
+    <div class="more-wrapper">
+      <a href="#" class="spelling-bt">拼单</a>
+      <div class="more-bt">
+        <i class="s-radius"></i>
+        <i class="s-radius"></i>
+        <i class="s-radius"></i>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+// @Component
+export default class TopWrapper extends Vue {}
+</script>
+
+<style scoped>
+.top-wrapper {
+  position: relative;
+}
+
+.top-wrapper .back-wrapper {
+  position: absolute;
+  left: 0; top: 0;
+  width: 50px;
+  height: 31px;
+  padding: 15px;
+  /* text-align: center; */
+  /* line-height: 31px; */
+}
+
+.top-wrapper .back-wrapper span {
+  display: inline-block;
+  color: white;
+}
+
+.top-wrapper .search-wrapper {
+  width: 100%;
+  height: 31px;
+  /* background: pink; */
+  padding: 0 104px 0 50px;
+  /* 设置盒子从边框开始计算*/
+  box-sizing: border-box;
+}
+
+.top-wrapper .search-wrapper .search-icon {
+  width: 28px;
+  height: 31px;
+  background: url(/static/star.png) no-repeat 11px center;
+  background-size: 13px 13px;
+  position: absolute;
+}
+
+.top-wrapper .search-wrapper .search-bar {
+  width: 100%;
+  height: 31px;
+  border: 0;
+  /* 设置盒子从边框开始计算*/
+  box-sizing: border-box;
+  background: #cdcdcc;
+  border-radius: 25px;
+  padding-left: 28px;
+  /* 去除选中时蓝色边框*/
+  outline: none;
+}
+
+.top-wrapper .more-wrapper {
+  width: 65px;
+  height: 24px;
+  /* background: orange; */
+  position: absolute;
+  right: 0;
+  top: 0;
+  padding: 7px 15px 0 24px;
+}
+
+.top-wrapper .more-wrapper .spelling-bt {
+  width: 30px;
+  height: 17px;
+  color: white;
+  line-height: 17px;
+  border: 1px solid white;
+  text-align: center;
+  float: left;
+  text-decoration: none;
+  font-size: 10px;
+}
+
+.top-wrapper .more-wrapper .more-bt {
+  float: right;
+  width: 20px;
+  height: 24px;
+  margin-left: 13px;
+  margin-top: 6px;
+}
+
+.top-wrapper .more-wrapper .more-bt .s-radius {
+  width: 3px;
+  height: 3px;
+  border-radius: 50%;
+  border: 1px solid white;
+  display: block;
+  float: left;
+  margin-right: 1px;
+}
+</style>
