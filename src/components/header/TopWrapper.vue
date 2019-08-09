b55b1bd6225bca233e5fd54ad9c9b81c5967f557
ChaosAlphard <Alphard450@outlook.com>
2019-08-09 21:19:49 +0800

    更新TopWrapper

--- src/components/header/TopWrapper.vue
+++ src/components/header/TopWrapper.vue
@@ -1,20 +1,18 @@
 <template>
   <div class="top-wrapper">
-    <div class="back-wrapper">
-      <span class="icon icon-arrow-left"></span>
+    <span class="back icon icon-arrow-left"></span>
+
+    <div class="search-wrapper">
+      <span class="mt-icon icon-search search-icon"></span>
+      <input type="text" class="search-bar" placeholder="搜索店内商品">
     </div>
 
-    <form class="search-wrapper">
-      <span class="search-icon"></span>
-      <input type="text" class="search-bar" placeholder="搜索店内商品">
-    </form>
-
     <div class="more-wrapper">
-      <a href="#" class="spelling-bt">拼单</a>
+      <span class="spelling-bt">拼单</span>
       <div class="more-bt">
-        <i class="s-radius"></i>
-        <i class="s-radius"></i>
-        <i class="s-radius"></i>
+        <span class="s-radius"></span>
+        <span class="s-radius"></span>
+        <span class="s-radius"></span>
       </div>
     </div>
   </div>
@@ -29,91 +27,93 @@ export default class TopWrapper extends Vue {}
 
 <style scoped>
 .top-wrapper {
-  position: relative;
+  display: flex;
+  flex-wrap: nowrap;
+  justify-content: space-around;
+  align-items: center;
+}
+.top-wrapper>div,.top-wrapper>span{
+  margin: 0 5px;
 }
 
-.top-wrapper .back-wrapper {
-  position: absolute;
-  left: 0; top: 0;
-  width: 50px;
-  height: 31px;
-  padding: 15px;
-  /* text-align: center; */
-  /* line-height: 31px; */
+.back {
+  color: #FFFFFF;
+  width: 30px;
+  height: 30px;
+  /* 设置不自适应压缩宽度 */
+  flex-shrink: 0;
+  line-height: 30px;
+  text-align: center;
 }
 
-.top-wrapper .back-wrapper span {
-  display: inline-block;
-  color: white;
-}
-
-.top-wrapper .search-wrapper {
+.search-wrapper {
   width: 100%;
-  height: 31px;
-  /* background: pink; */
-  padding: 0 104px 0 50px;
-  /* 设置盒子从边框开始计算*/
+  height: 30px;
   box-sizing: border-box;
+  background: #CCCCCC;
+  border-radius: 15px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
-.top-wrapper .search-wrapper .search-icon {
-  width: 28px;
-  height: 31px;
-  background: url(/static/star.png) no-repeat 11px center;
-  background-size: 13px 13px;
-  position: absolute;
+.search-wrapper .search-icon {
+  width: 30px;
+  height: 30px;
+  /* 设置不自适应压缩宽度 */
+  flex-shrink: 0;
+  text-align: center;
+  line-height: 30px;
+  font-size: 18px;
+  /* background: url("../../static/search.png") no-repeat center center; */
+  /* background-size: 15px 15px; */
 }
 
-.top-wrapper .search-wrapper .search-bar {
+.search-wrapper .search-bar {
   width: 100%;
-  height: 31px;
-  border: 0;
-  /* 设置盒子从边框开始计算*/
+  height: 30px;
+  padding-left: 2px;
+  padding-right: 10px;
   box-sizing: border-box;
-  background: #cdcdcc;
-  border-radius: 25px;
-  padding-left: 28px;
-  /* 去除选中时蓝色边框*/
+  background-color: transparent;
+  border: none;
+  /* 去除选中时边框*/
   outline: none;
 }
 
-.top-wrapper .more-wrapper {
-  width: 65px;
-  height: 24px;
-  /* background: orange; */
-  position: absolute;
-  right: 0;
-  top: 0;
-  padding: 7px 15px 0 24px;
+.more-wrapper {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 75px;
+  height: 30px;
+  /* 设置不自适应压缩宽度 */
+  flex-shrink: 0;
 }
 
-.top-wrapper .more-wrapper .spelling-bt {
+.more-wrapper .spelling-bt {
   width: 30px;
-  height: 17px;
-  color: white;
-  line-height: 17px;
-  border: 1px solid white;
-  text-align: center;
-  float: left;
-  text-decoration: none;
   font-size: 10px;
+  padding: 4px 4px;
+  color: #FFF;
+  border: 1px solid #FFF;
+  text-decoration: none;
+  text-align: center;
 }
 
-.top-wrapper .more-wrapper .more-bt {
-  float: right;
-  width: 20px;
-  height: 24px;
-  margin-left: 13px;
-  margin-top: 6px;
+.more-wrapper .more-bt {
+  width: 30px;
+  height: 20px;
+  line-height: 15px;
+  text-align: center;
 }
 
-.top-wrapper .more-wrapper .more-bt .s-radius {
+.more-wrapper .more-bt .s-radius {
+  display: inline-block;
   width: 3px;
   height: 3px;
   border-radius: 50%;
   border: 1px solid white;
-  display: block;
-  float: left;
-  margin-right: 1px;
+  margin: 1px;
 }
 </style>
