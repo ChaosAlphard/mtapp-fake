7abf2a90ffa376eb9f4a32b9509aef14861ec913
ChaosAlphard <Alphard450@outlook.com>
2019-08-09 21:20:04 +0800

    更新ContentWrapper

--- src/components/header/ContentWrapper.vue
+++ src/components/header/ContentWrapper.vue
@@ -1,65 +1,94 @@
 <template>
   <div class="content-wrapper">
-    <div class="ico" :style="headLogoStyle"></div>
-    <div class="name">
-      <h3>{{poiInfo.name}}</h3>
-    </div>
-    <div class="collect">
-      <img src="@/static/star.png">
-      <span>收藏</span>
+    <div class="logo" :style="logoStyle"></div>
+
+    <span class="name">{{name}}</span>
+
+    <div class="whitespace"></div>
+
+    <div class="collect" @click="toggleFav()">
+      <span class="mt-icon star" :class="favClass"></span>
+      <span class="fav">收藏</span>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Vue, Component } from 'vue-property-decorator'
+import { Vue, Component, Prop } from 'vue-property-decorator'
 
 @Component
-export default class ContentWrapper extends Vue {}
+export default class ContentWrapper extends Vue {
+  @Prop() readonly name!: string
+  @Prop() readonly logoStyle!: string
+
+  private favClass = {
+    'icon-star': true,
+    'icon-star-full': false
+  }
+
+  private toggleFav(): void {
+    this.favClass['icon-star'] = !this.favClass['icon-star']
+    this.favClass['icon-star-full'] = !this.favClass['icon-star-full']
+  }
+}
 </script>
 
 <style scoped>
 .content-wrapper {
-  padding: 17px 10px 11px;
-  height: 50px;
+  position: relative;
+  width: 100%;
+  height: 80px;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 15px 10px;
+  box-sizing: border-box;
+}
+.content-wrapper>div,.content-wrapper>span{
+  margin: 0 5px;
 }
 
-.content-wrapper .ico {
+.logo {
   width: 50px;
   height: 50px;
-  background-size: 135% 100%;
-  background-position: center;
-  border-radius: 5px;
-  float: left;
+  flex-shrink: 0;
+  background-size: cover;
+  background-position: center center;
+  border-radius: 8px;
 }
 
-.content-wrapper .name {
-  float: left;
-  padding: 18px 0 0 12px;
-}
-
-.content-wrapper .name h3 {
-  font-size: 16px;
+.name {
+  flex-shrink: 0;
+  font-size: 18px;
   font-weight: bold;
-  color: white;
+  color: #FFF;
 }
 
-.content-wrapper .collect {
-  width: 25px;
-  height: 37px;
-  float: right;
-  text-align: center;
-  padding-top: 6px;
+/* 占位撑开宽度 */
+.whitespace {
+  width: 100%;
 }
 
-.content-wrapper .collect img {
+.collect {
+  flex-shrink: 0;
+  width: 30px;
+  height: 40px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  color: #FFF;
+}
+
+.collect .star {
+  display: block;
   width: 20px;
   height: 20px;
+  font-size: 20px;
 }
 
-.content-wrapper .collect span {
-  margin-top: 7px;
-  color: white;
-  font-size: 11px;
+.collect .fav {
+  display: block;
+  font-size: 12px;
 }
 </style>
